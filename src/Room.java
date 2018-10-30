b39d95b9b67421a1b86792eb3ded6660f6977f0c
BT_1N3_23 <jhan3537@bths.edu>
2018-10-30 11:45:23 -0400

    drgdrg

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,8 @@
 public class Room {
+    Profile occupant; int xLoc; int yLoc;
+
+    public Room(int x, int y) {
+        xLoc = x; yLoc = y;
+    }
+
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
 public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Welcome, enter your name.");
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        Profile you = new Profile(input, 0, 0);
+        int fieldLength = 25; int fieldWidth = 25;
+
+        Room[][] field = new Room[fieldLength][fieldWidth];
+
+        for (int i = 0; i < fieldLength; i ++) {
+            for (int j = 0; j < fieldWidth; j ++) {
+                field[i][j] = new Room(i, j);
+            }
+        }
+    }
 
 }
