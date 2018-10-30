a7704d5b30b39791dc09560546be5433e002230b
BT_1N3_23 <jhan3537@bths.edu>
2018-10-30 11:28:43 -0400

    drgdrg

--- src/Profile.java
+++ src/Profile.java
@@ -1,3 +1,26 @@
 public class Profile {
-    
+    String name;
+    int xLoc; int yLoc;
+
+    public Profile(String name, int x, int y) {
+        this.name = name;
+        this.xLoc = x;
+        this.yLoc = y;
+    }
+
+    public void move(int x, int y) {
+        xLoc += x;
+        yLoc += y;
+    }
+
+    public void setLocation(int x, int y){
+        xLoc = x;
+        yLoc = y;
+    }
+
+    public int[] returnLocation() {
+        int[] z = {xLoc, yLoc};
+        return z;
+    }
+
 }
